28a33be01b2d200344feceefd86a17968b5f8499
TakanoriShima <quark2galaxy@gmail.com>
2024-04-15 02:45:13 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
@@ -14,6 +17,58 @@ class TaskController extends Controller
      */
     public function list()
     {
-        return view('task.list');
+        
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 2;
+        
+        // 一覧の取得
+        $list = TaskModel::where('user_id', Auth::id())
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->paginate($per_page);
+                        // ->get();
+        /*
+        $sql = TaskModel::where('user_id', Auth::id())
+                        ->orderBy('priority', 'DESC')
+                        ->orderBy('period')
+                        ->orderBy('created_at')
+                        ->toSql();
+        //echo "<pre>\n"; var_dump($sql, $list); exit;
+        var_dump($sql);
+        */
+        
+        return view('task.list', ['list' => $list]);
+    }
+    
+    /**
+     * タスクの新規登録
+     */
+    public function register(TaskRegisterPostRequest $request)
+    {
+        // validate済みのデータの取得
+        $datum = $request->validated();
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+
+        // テーブルへのINSERT
+        try {
+            $r = TaskModel::create($datum);
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+
+        // タスク登録成功
+        $request->session()->flash('front.task_register_success', true);
+
+        // リダイレクト
+        return redirect('/task/list');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,18 @@ use App\Http\Controllers\TaskController;
 */
 
 // タスク管理システム
-Route::get('/', [AuthController::class, 'index']);
-Route::get('/task/list', [TaskController::class, 'list']);
+Route::get('/', [AuthController::class, 'index'])->name('front.index');
+Route::post('/login', [AuthController::class, 'login']);
+// 認可処理
+Route::middleware(['auth'])->group(function () {
+    Route::get('/task/list', [TaskController::class, 'list']);
+    Route::post('/task/register', [TaskController::class, 'register']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
 
 // テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
+// form入力テスト用
+Route::get('/test', [TestController::class, 'index']);
+Route::post('/test/input', [TestController::class, 'input']);
